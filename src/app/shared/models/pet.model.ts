615db135c8127bc4d9e43a1c4cf0837fb935667e
Carlos Navarro <cnavarro3196@gmail.com>
2019-02-24 15:40:41 -0600

    feat(models): add category model

---
+++ src/app/shared/models/pet.model.ts
@@ -0,0 +1,10 @@
+import { Category } from './category.model';
+
+export class Pet {
+    id: number;
+    name: string;
+    category: Category;
+    photoUrls: string[];
+    tags: string[];
+    status: number;
+}
