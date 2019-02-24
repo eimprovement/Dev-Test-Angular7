2cf1bf510e401b31866dd7c823e765445505d96c
Carlos Navarro <cnavarro3196@gmail.com>
2019-02-24 15:18:25 -0600

    feat(pets-module): add pets routing module

--- src/app/pets/pets.module.ts
+++ src/app/pets/pets.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PetsRoutingModule } from './pets-routing.module';
+
 @NgModule({
   declarations: [],
-  imports: [CommonModule]
+  imports: [CommonModule, PetsRoutingModule]
 })
 export class PetsModule {}
