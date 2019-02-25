19ffe36ae28453756c01800f8f4c229d8662fdd1
Carlos Navarro <cnavarro3196@gmail.com>
2019-02-24 19:03:17 -0600

    feat(routes): add pets route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { Shell } from '@app/shell/shell.service';
 
 const routes: Routes = [
   Shell.childRoutes([{ path: 'about', loadChildren: 'app/about/about.module#AboutModule' }]),
+  Shell.childRoutes([{ path: 'pets', loadChildren: 'app/pets/pets.module#PetsModule' }]),
   // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
